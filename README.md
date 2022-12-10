39b50693a3ab051e759c04a4aa1ba6411298bc31
loreb92 <lorenzobetti92@gmail.com>
2022-12-10 16:47:21 +0100

    update repo

--- README.md
+++ README.md
@@ -6,3 +6,7 @@ This repository contains the scripts used for the final project of the Introduct
 ```
 python run_experiment.py --input-params-file config/experiment_only_experts_ER.json --results-folder results/ --n-workers 7 --log-fold logs/ --initial-seed 1 --n-runs-per-param 10 
 ```
+
+Empirical network: The Copenhagen Networks Study interaction data, Facebook. Downloaded from: https://figshare.com/articles/dataset/The_Copenhagen_Networks_Study_interaction_data/7267433/1
+
+Then unzip file.
--- src/utils_network.py
+++ src/utils_network.py
@@ -26,8 +26,12 @@ def generate_network(N, Ne, network_params, seed=None):
         _, average_degree = network_params.split("_")
         average_degree = int(average_degree)
         G = generate_ER_network(N, average_degree, seed=seed)
+    elif network_params.startswith('empirical'):
+        path = network_params.split("_", maxsplit=1)[-1]
+        G = nx.read_edgelist(path, delimiter=",", comments='#', nodetype=int)
+        G = nx.relabel_nodes(G, {node:i for i, node in enumerate(G.nodes())})
     else:
-        raise NotImplementedError(f"The generation of {network_dict['network_model']} network model is not implemented.")
+        raise NotImplementedError(f"The generation of {network_params} network model is not implemented.")
 
     # get index of agents and split normal agents and experts
     # rename nodes in such a way that nodes 0, 1, ..., Na - 1 are the normal agents and Na, Na + 1, ... N are the experts
